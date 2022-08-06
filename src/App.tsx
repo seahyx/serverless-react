379b21af6717db0d85b92a5d54eced9a79106130
Seah Ying Xiang <seahyx@gmail.com>
2022-08-07 01:01:29 +0800

    Configured tailwindcss and added test components

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,27 @@ import "./App.css";
 import * as React from "react";
 
 import useConfig from "./components/useConfig";
-import logo from "./logo.svg";
 
 /**
  * Our Web Application
  */
 export default function App() {
-  const config = useConfig();
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to {config.app.TITLE}</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.tsx</code> and save to reload.
-      </p>
-    </div>
-  );
+	const config = useConfig();
+	return (
+		<div className="w-full h-screen bg-gray-200">
+			<RetardedButton />
+			<RetardedButton />
+		</div>
+	);
+}
+
+function RetardedButton() {
+	return (
+		<div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 hover:bg-gray-100 active:bg-gray-200">
+			<div>
+				<div className="text-xl font-medium text-primary">ChitChat</div>
+				<p className="text-slate-500">You have a new message!</p>
+			</div>
+		</div>
+	)
 }
--- src/browser/index.tsx
+++ src/browser/index.tsx
@@ -2,7 +2,7 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-import "./index.css";
+import "./output.css";
 
 /**
  * Frontend code running in browser
@@ -19,15 +19,15 @@ delete (window as any).__CONFIG__;
 
 /** Components added here will _only_ be loaded in the web browser, never for server-side rendering */
 const render = () => {
-  hydrate(
-    <>
-      {/* The configuration is the outmost component. This allows us to read the configuration even in the theme */}
-      <ConfigContext.Provider value={config}>
-        <App />
-      </ConfigContext.Provider>
-    </>,
-    document.getElementById("root"),
-  );
+	hydrate(
+		<>
+			{/* The configuration is the outmost component. This allows us to read the configuration even in the theme */}
+			<ConfigContext.Provider value={config}>
+				<App />
+			</ConfigContext.Provider>
+		</>,
+		document.getElementById("root"),
+	);
 };
 
 render();
